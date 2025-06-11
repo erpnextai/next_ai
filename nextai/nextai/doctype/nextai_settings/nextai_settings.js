8372ea64fd6f473ed84a676c4da759c5db18da4f
Antony-M1 <antonykumar15898@gmail.com>
2025-06-11 03:01:49 +0000

    NextAI settings

---
+++ nextai/nextai/doctype/nextai_settings/nextai_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, antonykumar15898@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('NextAI Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
