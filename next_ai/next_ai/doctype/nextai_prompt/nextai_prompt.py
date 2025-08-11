87601a8ac881ad692a181251e62eacd1189ec272
Sam <antony.bizmap@gmail.com>
2025-08-11 17:35:46 +0000

    Filder Field Values

--- next_ai/next_ai/doctype/nextai_prompt/nextai_prompt.py
+++ next_ai/next_ai/doctype/nextai_prompt/nextai_prompt.py
@@ -1,8 +1,44 @@
 # Copyright (c) 2025, Antony Praveenkumar Moses and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class NextAIPrompt(Document):
 	pass
+
+
+@frappe.whitelist(methods=['POST'])
+def get_field_type_fielname(ref_doctype:str):
+	"""
+	Get the field details of the given doctype.
+	"""
+	query = """
+	SELECT
+		parent AS doc_type,
+		label,
+		fieldname,
+		fieldtype
+	FROM
+		`tabDocField`
+	WHERE
+		parent = '{ref_doctype}'
+		AND fieldtype IN ('Markdown Editor', 'Small Text', 'Long Text', 'Text', 'HTML Editor', 'Text Editor', 'Code')
+
+	UNION ALL
+
+	SELECT
+		dt AS doc_type,
+		label,
+		fieldname,
+		fieldtype
+	FROM
+		`tabCustom Field`
+	WHERE
+		dt = '{ref_doctype}'
+		AND fieldtype IN ('Markdown Editor', 'Small Text', 'Long Text', 'Text', 'HTML Editor', 'Text Editor', 'Code')
+	ORDER BY label;
+	""".format(ref_doctype=ref_doctype)
+
+	data = frappe.db.sql(query, as_dict=True)
+	return data
