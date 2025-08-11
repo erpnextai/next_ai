79ceec07492431ec3860a7d08efafc41669e7979
Sam <antony.bizmap@gmail.com>
2025-08-11 15:51:55 +0000

    NextAI Prompt

---
+++ next_ai/next_ai/doctype/nextai_prompt/nextai_prompt.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Antony Praveenkumar Moses and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('NextAI Prompt', {
+	// refresh: function(frm) {
+
+	// }
+});
